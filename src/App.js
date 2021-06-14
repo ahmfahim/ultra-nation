39838a691de339c00c39b0b5f92ac4116dab9858
ahmfahim <fahimmorshed2019@gmail.com>
2021-06-14 13:06:35 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import Country from './components/Country';
+import Country from './components/Country/Country';
+import Cart from './components/Cart/Cart';
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     fetch('https://restcountries.eu/rest/v2/all')
@@ -12,11 +14,18 @@ function App() {
       .catch(error => console.log(error))
   }, [])
 
+  // event handler
+  const handleAddCountry = (country) => {
+    const newCart = [...cart, country];
+    setCart(newCart);
+  }
   return (
     <div className="App">
       <h1>Country Loaded: {countries.length}</h1>
+      <h3>country added:{cart.length} </h3>
+      <Cart cart={cart}></Cart>
       {
-        countries.map(country=><Country countryInfo={country}></Country>)
+        countries.map(country=><Country countryInfo={country} handleAddCountry={handleAddCountry}></Country>)
       }
     </div>
   );
--- src/components/Country/Country.js
+++ src/components/Country/Country.js
@@ -3,11 +3,13 @@ import './Country.css'
 
 const Country = (props) => {
     const { flag, name, capital } = props.countryInfo;
+    const handleAddCountry = props.handleAddCountry;
     return (
         <div className="country-div">
             <img src={flag} alt=""/>
             <h3>{name}</h3>
             <h5>{capital}</h5>
+            <button onClick={() => handleAddCountry(props.countryInfo)}>Add Country</button>
         </div>
     );
 };
